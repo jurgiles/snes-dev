adb62c8aef568849bcc465b7166ed10a010c22ff
Johny Urgiles <johny.urgiles@gmail.com>
2017-12-03 17:55:01 +0100

    Update build script to launch emulator and remove smc files

--- build_and_run.sh
+++ build_and_run.sh
@@ -3,4 +3,4 @@
 wla-65816 -v helloworld.asm
 wlalink -v helloworld.link helloworld.smc
 
-open -a /Applications/Snes9x.app/Contents/MacOS/Snes9x/ helloworld.smc
+open -aW /Applications/Snes9x.app/Contents/MacOS/Snes9x/ helloworld.smc
